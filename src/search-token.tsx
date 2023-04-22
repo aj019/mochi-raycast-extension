5f7cf4d76f236c94d288c1697baf66dcc6d93908
anujgupta <anujgupta019@gmail.com>
2023-04-22 09:27:43 +0530

    Added Details section for token and provided link to coin gecko page

--- src/search-token.tsx
+++ src/search-token.tsx
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from 'react'
-import {List} from '@raycast/api'
+import {Action, ActionPanel, List} from '@raycast/api'
 import {fetchTokens} from './api'
 
 export default function SearchToken() {
@@ -21,8 +21,36 @@ export default function SearchToken() {
    },[])
 
   return (
-    <List filtering={true}>
-        {tokens.map(token => <List.Item key={token.id} title={`${token.name} (${token.symbol})`} />)}
-    </List>
+      <List filtering={true} isShowingDetail>
+          {tokens.map(token =>
+              <List.Item 
+                key={token.id}
+                title={`${token.name} (${token.symbol})`} 
+                detail={
+                    <List.Item.Detail
+                      metadata={
+                        <List.Item.Detail.Metadata>
+                            <List.Item.Detail.Metadata.Label title="ID" text={`${token.id}`} />
+                            <List.Item.Detail.Metadata.Separator />
+                            <List.Item.Detail.Metadata.Label title="NAME" text={`${token.name}`} />
+                            <List.Item.Detail.Metadata.Separator />
+                            <List.Item.Detail.Metadata.Label title="ADDRESS" text={`${token.address}`} />
+                            <List.Item.Detail.Metadata.Separator />
+                            <List.Item.Detail.Metadata.Label title="SYMBOL" text={`${token.symbol}`} />
+                            <List.Item.Detail.Metadata.Separator />
+                            <List.Item.Detail.Metadata.Label title="CHAIN" text={`${token.chain.name}`} />
+                            <List.Item.Detail.Metadata.Separator />
+                            <List.Item.Detail.Metadata.Link
+                            title="Checkout on CoinGecko"
+                            target={`https://www.coingecko.com/en/coins/${token.coin_gecko_id}`}
+                            text={`${token.coin_gecko_id}`}
+                            />
+                        </List.Item.Detail.Metadata>
+                      }
+                    />
+                  }
+               />)
+        }
+      </List>
   )
 }
